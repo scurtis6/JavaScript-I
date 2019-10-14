18e1cbeda5a8ef9e43b755fd702409d40e73974b
Your Name <you@example.com>
2019-10-14 19:37:52 -0400

    finshed project

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -39,3 +39,8 @@ console.log(subtract(1,2))
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple)
